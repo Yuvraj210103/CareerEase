9b678af62dbd06e2dd0f644e2cbb03d56c905706
Yuvraj210103 <ys146228@gmail.com>
2024-11-23 15:22:06 +0530

    refactor: logo refactored

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -5,9 +5,9 @@ const Nav = () => {
     <div className="flex items-center justify-center w-full h-full bg-surface shadow-lg border-b-[2px] border-inputBorder">
       <div className="flex items-center w-full px-8 py-4 justify-between max-w-[1280px]">
         <div className="flex items-center gap-10">
-          <div className="flex items-center gap-1">
-            <img src="assets/logo.png" alt="" className="size-10" />
-            <div className="font-bold text-lg">CareerEase</div>
+          <div className="flex items-center">
+            <img src="/logo.png" alt="" className="h-[50px] w-[65px]" />
+            <div className="font-bold text-xl">CareerEase</div>
           </div>
           <div className="flex items-center gap-8">
             <div className="text-lg font-semibold cursor-pointer">Home</div>
