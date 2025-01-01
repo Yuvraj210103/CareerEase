68e25c64d7d8ac468d2c7135b7bd877ac2c83342
Yuvraj210103 <ys146228@gmail.com>
2025-01-01 17:32:07 +0530

    feat: implemented resume download feature

---
+++ src/API/HtmlToPdf.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_SERVER_BASE_URL || "http://localhost:3000";
+
+export const htmlToPdf = ({
+  file_name,
+  html,
+  pdf_options,
+}: {
+  html: string;
+  file_name: string;
+  pdf_options?: {
+    format: "A3" | "A4" | "A4" | "Letter";
+    orientation: "portrait" | "landscape";
+  };
+}) => {
+  return axios.post(
+    `${baseUrl}/api/html_to_pdf`,
+    {
+      html,
+      file_name,
+      pdf_options: pdf_options
+        ? pdf_options
+        : { format: "A4", orientation: "portrait" },
+    },
+    { responseType: "blob" }
+  );
+};
--- src/pages/user/resume_templates/ResumeTemplates.tsx
+++ src/pages/user/resume_templates/ResumeTemplates.tsx
@@ -1,9 +1,12 @@
 import { IoCreateOutline } from "react-icons/io5";
-import { useAuthState } from "../../../store";
+import { useAuthState, useUIState } from "../../../store";
 import { templateGenerate } from "../../../utilities/resume_templates/TemplateGenerate";
 import { useEffect, useState } from "react";
 import DbUser from "../../../firebase/DB/DbUser";
 import { errorHandler } from "../../../utilities/CustomError";
+import Button from "../../../component/common/button/Button";
+import { htmlToPdf } from "../../../API/HtmlToPdf";
+import { downloadPdf } from "../../../utilities/downloadPdf";
 
 const templates = [
   {
@@ -22,6 +25,8 @@ const ResumeTemplates = () => {
   const [selectedTemplate, setSelectedTemplate] = useState(
     settings?.SettingSelectedResumeTemplate || 1
   );
+
+  const { setLoading } = useUIState();
 
   useEffect(() => {
     if (
@@ -60,28 +65,61 @@ const ResumeTemplates = () => {
     doc.close();
   };
 
+  const handleDownloadClick = async () => {
+    if (!userProfile || !settings) return;
+    try {
+      setLoading(true);
+
+      const html = templateGenerate({
+        UserProfile: userProfile,
+        selectedTemplate,
+      });
+
+      const response = await htmlToPdf({ file_name: "resume.pdf", html });
+
+      downloadPdf(response, "resume.pdf");
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      errorHandler(error);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-start h-[calc(100vh-95px)] gap-4">
-        <div className="bg-surface h-full w-[20%] py-4 flex flex-col">
-          <div className="font-bold text-lg text-textSecondary mb-4 px-4">
-            Select Template
+        <div className="bg-surface h-full w-[20%] py-4 flex flex-col ">
+          <div className="flex flex-col h-full">
+            <div className="font-bold text-lg text-textSecondary mb-4 px-4">
+              Select Template
+            </div>
+
+            {templates.map((res, idx) => {
+              return (
+                <div
+                  key={res.id}
+                  className={`py-4 border-b px-4 font-semibold  cursor-pointer duration-200 ${
+                    idx + 1 === selectedTemplate
+                      ? "bg-onHoverBg"
+                      : "hover:bg-onHoverBg/50"
+                  }`}
+                  onClick={() => setSelectedTemplate(idx + 1)}
+                >
+                  {res.title}
+                </div>
+              );
+            })}
           </div>
-          {templates.map((res, idx) => {
-            return (
-              <div
-                key={res.id}
-                className={`py-4 border-b px-4 font-semibold  cursor-pointer duration-200 ${
-                  idx + 1 === selectedTemplate
-                    ? "bg-onHoverBg"
-                    : "hover:bg-onHoverBg/50"
-                }`}
-                onClick={() => setSelectedTemplate(idx + 1)}
-              >
-                {res.title}
-              </div>
-            );
-          })}
+          <div className="px-4 w-full flex">
+            <Button
+              label="Download"
+              type="black"
+              className="w-full"
+              onClick={handleDownloadClick}
+            />
+          </div>
         </div>
         <div className="bg-surface h-full w-[80%] p-4">
           {userProfile ? (
